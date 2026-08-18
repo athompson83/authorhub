d1ef572b12fcd72ae0f66f8b3a281c063f1365fc
athompson83 <paramedicine101@gmail.com>
2026-08-18 11:42:54 -0400

    feat: export auth domain

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,4 @@
+export { can } from './permissions';
+export type { BookPermission } from './permissions';
+export { organizationRoles, bookRoles } from './roles';
+export type { OrganizationRole, BookRole } from './roles';
