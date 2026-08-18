b97b06e79c63bc358d2cb7c1b06da96274e78a3f
athompson83 <paramedicine101@gmail.com>
2026-08-18 11:41:28 -0400

    feat: export manuscript domain primitives

---
+++ packages/manuscript/src/index.ts
@@ -0,0 +1,7 @@
+export { createBlockId, isBlockId } from './block-id';
+export {
+  blockIdSchema,
+  manuscriptBlockSchema,
+  manuscriptDocumentSchema,
+} from './schema';
+export type { ManuscriptBlock, ManuscriptDocument } from './schema';
